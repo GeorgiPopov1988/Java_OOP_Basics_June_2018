f12c99f6643e961ab4bae6ccee403c4d2ae2b7a8
Georgi Popov <georgipopov88@gmail.com>
2018-06-18 23:15:15 +0300

    Class box - done.

---
+++ src/encapsulation/exercises/class_box/Main.java
@@ -0,0 +1,27 @@
+package encapsulation.exercises.class_box;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+  
+    double length = Double.parseDouble(reader.readLine());
+    double width = Double.parseDouble(reader.readLine());
+    double height = Double.parseDouble(reader.readLine());
+    Box box = null;
+    try {
+      box = new Box(length, width, height);
+    } catch (NumberFormatException nfe) {
+      System.out.println(nfe.getMessage());
+    }
+    
+    if (box != null) {
+      System.out.printf("Surface Area - %.2f%n", box.getSurfaceArea());
+      System.out.printf("Lateral Surface Area - %.2f%n", box.getLateralSurfaceArea());
+      System.out.printf("Volume - %.2f%n", box.getVolume());
+    }
+  }
+}
