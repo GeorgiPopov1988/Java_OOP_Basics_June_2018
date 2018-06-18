2bd83902267b142207055425710bec5e95d017df
Georgi Popov <georgipopov88@gmail.com>
2018-06-18 22:36:18 +0300

    Encapsulation Lab - done. - Sort by name and age. - Salary increase. - Validation data. - First and reserve team.

---
+++ src/encapsulation/lab/sort_by_name_and_age/Main.java
@@ -0,0 +1,61 @@
+package encapsulation.lab.sort_by_name_and_age;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(reader.readLine());
+    
+    List<Person> people = new ArrayList<>();
+    
+    for (int i = 0; i < n; i++) {
+      Person person = null;
+      String[] input = reader.readLine().split(" ");
+      String firstName = input[0];
+      String lastName = input[1];
+      int age = Integer.parseInt(input[2]);
+      double salary = Double.parseDouble(input[3]);
+      
+      try {
+        person = new Person(firstName, lastName, age, salary);
+        people.add(person);
+        
+      } catch (IllegalArgumentException iae) {
+        System.out.println(iae.getMessage());
+      }
+    }
+    
+    // Sort in alphabetic order
+    //Collections.sort(people, (firstPerson, secondPerson) -> {
+    //  int sComp = firstPerson.getFirstName().compareTo(secondPerson.getFirstName());
+    //
+    //  if (sComp != 0) {
+    //    return sComp;
+    //  } else {
+    //    return Integer.compare(firstPerson.getAge(), secondPerson.getAge());
+    //  }
+    //});
+    
+    //double bonus = Double.parseDouble(reader.readLine());
+    //
+    //for (Person person : people) {
+    //  person.increaseSalary(bonus);
+    //  System.out.println(person.toString());
+    //}
+    String teamName = reader.readLine();
+    Team team = new Team(teamName);
+  
+    for (Person person : people) {
+      team.addPlayer(person);
+    }
+  
+    System.out.println("First team have " + team.getFirstTeam().size() + " players");
+    System.out.println("Reserve team have " + team.getReserveTeam().size() + " players");
+    
+  }
+}
---
+++ src/encapsulation/lab/sort_by_name_and_age/Person.java
@@ -0,0 +1,74 @@
+package encapsulation.lab.sort_by_name_and_age;
+
+public class Person {
+  private String firstName;
+  private String lastName;
+  private Integer age;
+  private Double salary;
+  
+  public Person(String firstName, String lastName, Integer age, Double salary) {
+    setFirstName(firstName);
+    setLastName(lastName);
+    setAge(age);
+    setSalary(salary);
+  }
+  
+  private void setFirstName(String firstName) {
+    if (firstName.length() < 3) {
+      throw new IllegalArgumentException("First name cannot be less than 3 symbols");
+    }
+    this.firstName = firstName;
+  }
+  
+  public String getFirstName() {
+    return this.firstName;
+  }
+  
+  private void setLastName(String lastName) {
+    if (lastName.length() < 3) {
+      throw new IllegalArgumentException("Last name cannot be less than 3 symbols");
+    }
+    this.lastName = lastName;
+  }
+  
+  private String getLastName() {
+    return this.lastName;
+  }
+  
+  private void setAge(Integer age) {
+    if (age < 0) {
+      throw new IllegalArgumentException("Age cannot be zero or negative integer");
+    }
+    this.age = age;
+  }
+  
+  public Integer getAge() {
+    return this.age;
+  }
+  
+  private void setSalary(Double salary) {
+    if (salary < 460) {
+      throw new IllegalArgumentException("Salary cannot be less than 460 leva");
+    }
+    this.salary = salary;
+  }
+  
+  private Double getSalary() {
+    return this.salary;
+  }
+  
+  public void increaseSalary(Double bonus) {
+    if (this.getAge() > 30) {
+      this.salary = this.salary + (this.salary * bonus / 100);
+    } else {
+      this.salary = this.salary + (this.salary * bonus / 200);
+    }
+  }
+  
+  @Override
+  public String toString() {
+    //return String.format("%s %s gets %s leva"
+            //, this.getFirstName(), this.getLastName(), Double.toString(this.getSalary()));
+    return this.firstName + " "+ this.lastName + " gets " + this.getSalary() + " leva";
+  }
+}
