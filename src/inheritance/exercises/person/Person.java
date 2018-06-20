3e56e84b0bdd2386133890df277b8398b6c1bff7
Georgi Popov <georgipopov88@gmail.com>
2018-06-20 23:15:14 +0300

    Person - done.

---
+++ src/inheritance/exercises/person/Person.java
@@ -0,0 +1,40 @@
+package inheritance.exercises.person;
+
+public class Person {
+  private String name;
+  private int age;
+  
+  public Person(String name, int age) {
+    this.setName(name);
+    this.setAge(age);
+  }
+  
+  public String getName() {
+    return this.name;
+  }
+  
+  private void setName(String name) {
+    if (name.length() < 3) {
+      throw new IllegalArgumentException("Name's length should not be less than 3 symbols!");
+    } else {
+      this.name = name;
+    }
+  }
+  
+  public int getAge() {
+    return this.age;
+  }
+  
+  protected void setAge(int age) {
+    if (age < 0) {
+      throw new IllegalArgumentException("Age must be positive!");
+    } else {
+      this.age = age;
+    }
+  }
+  
+  @Override
+  public String toString() {
+    return String.format("Name: %s, Age: %d", this.getName(), this.getAge());
+  }
+}
