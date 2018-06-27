caa83194c4a92e58dd75ec86e68beba8d27a2384
Georgi Popov <georgipopov88@gmail.com>
2018-06-27 21:36:34 +0300

    Define person, multiple implementation - done.

---
+++ src/interfaces_and_abstraction/exercises/define_person_multiple_implementation/Identifiable.java
@@ -0,0 +1,6 @@
+package interfaces_and_abstraction.exercises.define_person_multiple_implementation;
+
+public interface Identifiable {
+  
+  String getId();
+}
---
+++ src/interfaces_and_abstraction/exercises/define_person_multiple_implementation/Person.java
@@ -0,0 +1,8 @@
+package interfaces_and_abstraction.exercises.define_person_multiple_implementation;
+
+public interface Person {
+  
+  String getName();
+  int getAge();
+  
+}
